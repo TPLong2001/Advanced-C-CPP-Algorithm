e61254440b486a5de92f47a65e8158e1abaa4426
Long <longdz2308@gmail.com>
2025-03-06 13:17:49 +0700

    doc

--- Mini_Project1/Automotive_Module_Manager/main.c
+++ Mini_Project1/Automotive_Module_Manager/main.c
@@ -6,13 +6,11 @@
 int main(){
     uint8_t exit = 0;
 BEGIN:
-
     Module module1 = {1, "ECU", 0, NULL};
     Module module2 = {2, "ABS", 0, NULL};
     Module module3 = {3, "LIGHT", 0, NULL};
     Module module4 = {4, "AIR CONDITIONER", 0, NULL};
     Module module5 = {5, "TEMPERATURE SENSOR", 0, NULL};
-
 
     addModule(&module1);
     addModule(&module2);
@@ -29,7 +27,6 @@ BEGIN:
     // moduleStatus(&moduleList[0]);
     // moduleStatus(&moduleList[1]);
     // moduleStatus(&moduleList[2]);
-
     while (1)
     {
         // làm việc với module
